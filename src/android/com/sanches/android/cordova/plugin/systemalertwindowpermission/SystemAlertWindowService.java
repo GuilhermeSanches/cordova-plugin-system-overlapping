8dc904a1524ae3d3ace129da1f8f7efa9efa3843
Helder Rodrigues <helderhenrique@MacBook-Pro-de-Helder.local>
2018-12-21 16:21:06 -0200

    remoçao da view depois de 30 segs

--- src/android/com/sanches/android/cordova/plugin/systemalertwindowpermission/SystemAlertWindowService.java
+++ src/android/com/sanches/android/cordova/plugin/systemalertwindowpermission/SystemAlertWindowService.java
@@ -4,6 +4,8 @@ import android.app.Service;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
 import android.view.Gravity;
 import android.view.View;
@@ -18,37 +20,56 @@ public class SystemAlertWindowService extends Service implements View.OnClickLis
     private WindowManager windowManager;
     private ImageView chatHead;
     MediaPlayer mp;
-    @Override public IBinder onBind(Intent intent) {
+
+    @Override
+    public IBinder onBind(Intent intent) {
         // Not used
         return null;
     }
 
-
-    @Override public void onCreate() {
+    @Override
+    public void onCreate() {
 
         super.onCreate();
         mp = MediaPlayer.create(this, R.raw.alarm);
-        if(mp != null) mp.setLooping(false);
-        if(mp != null) mp.start();
+        if (mp != null)
+            mp.setLooping(false);
+        if (mp != null)
+            mp.start();
 
         windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
 
         chatHead = new ImageView(this);
         chatHead.setImageResource(R.drawable.android_head);
         implementClickListeners();
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_PHONE,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT);
+        WindowManager.LayoutParams params;
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            params = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.TYPE_PHONE,
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.TRANSLUCENT);
+        } else {
+            params = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.TRANSLUCENT);
+        }
 
         params.gravity = Gravity.TOP | Gravity.LEFT;
         params.x = 0;
         params.y = 100;
 
         windowManager.addView(chatHead, params);
-       
+
+        final long THIRTY_SEC = 30000;
+        Handler handler = new Handler();
+
+        final Runnable r = new Runnable() {
+            public void run() {
+                windowManager.removeViewImmediate(chatHead);
+            }
+        };
+
+        handler.postDelayed(r, THIRTY_SEC);
+
     }
 
     private void implementClickListeners() {
@@ -61,13 +82,14 @@ public class SystemAlertWindowService extends Service implements View.OnClickLis
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
 
-        //close the service and remove view from the view hierarchy
+        // close the service and remove view from the view hierarchy
         stopSelf();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (chatHead != null) windowManager.removeView(chatHead);
+        if (chatHead != null)
+            windowManager.removeView(chatHead);
     }
 }
